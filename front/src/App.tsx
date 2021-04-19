d58a8c04f10bd29041d0071a9227460eba290503
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-04-19 17:13:24 +0400

    feat(main): init front part

---
+++ front/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+const App: React.FC = (): JSX.Element => {
+  return <div>Hello webpack and react</div>;
+};
+
+export default App;
---
+++ front/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    proxy: {
+      "/api": "http://localhost:3001",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
+            // plugins: [
+            //   ["@babel/plugin-transform-runtime", { regenerator: true }],
+            //   ["@babel/plugin-proposal-class-properties", { loose: false }],
+            //   [
+            //     "import",
+            //     { libraryName: "antd", style: "css", libraryDirectory: "es" },
+            //     "antd",
+            //   ],
+            // ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
